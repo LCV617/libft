7cafaad25063eac0d40e16aac834f183d0e42e24
Emilien Waltz <ewaltz@z2r10p4.42lyon.fr>
2025-11-14 17:06:12 +0100

    added strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 16:45:27 by ewaltz            #+#    #+#             */
+/*   Updated: 2025/11/14 17:04:20 by ewaltz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char fu(unsigned int i, char c)
+{
+  return(c + i);
+}
+
+char *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+  unsigned int i;
+  char *dest;
+
+  i = 0;
+  dest = malloc((sizeof(char) * ft_strlen(s)) + 1);
+  if(!dest)
+    return(NULL);
+  while(s[i])
+  {
+    dest[i] = (*f)(i, s[i]);
+    i++;
+  }
+  return(dest);
+}
+
+
+int main()
+{
+  const char* str = "aaaaaa";
+
+  printf("%s", ft_strmapi(str, &fu));
+}
