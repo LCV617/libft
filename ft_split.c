f26419c647f6fe22fc336092d55149248d0ff411
Emilien Waltz <emilienwaltz@gmail.com>
2025-11-17 19:49:44 +0100

    added split

--- ft_split.c
+++ ft_split.c
@@ -1,13 +1,81 @@
-char** ft_split(const char* s, char c)
+#include <stdlib.h>
+
+static size_t	ft_word_count(char const *s, char c)
 {
-  char** dest;
-  
-  dest = malloc(sizeof(char) * );
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
 }
 
-
-int main()
+static char	*ft_word_dup(char const *s, size_t start, size_t end)
 {
+	char	*str;
+	size_t	i;
 
-
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
 }
+
+static void	ft_free_all(char **tab, size_t i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(tab[i]);
+	}
+	free(tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	if (!s)
+		return (NULL);
+	tab = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			tab[j] = ft_word_dup(s, start, i);
+			if (!tab[j])
+				return (ft_free_all(tab, j), NULL);
+			j++;
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
+}
+
